58249d4f7aa72e557535f207c77c76998c3f434a
kate-skip <u5pdAcpLsZF6MfqeYz5T>
2024-02-10 18:48:49 +0800

    FileUtil code update after UT

--- src/main/java/com/demo/wordcount/common/FileUtil.java
+++ src/main/java/com/demo/wordcount/common/FileUtil.java
@@ -5,10 +5,10 @@ import com.demo.wordcount.exception.FileDownloadException;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Locale;
 
@@ -17,7 +17,7 @@ import java.util.Locale;
 public class FileUtil {
     private static final String FILE_EXTENSION_TXT = ".txt";
 
-    public static boolean isTextFile(String filepath) {
+    public static boolean hasTxtFileExtension(String filepath) {
         if (filepath != null) {
             return filepath.toLowerCase(Locale.getDefault()).endsWith(FILE_EXTENSION_TXT);
         } else {
@@ -26,12 +26,12 @@ public class FileUtil {
     }
 
     public static Path createDirectory(String mainDirectory, String subdirectory) throws FileCreationException {
-        Path currentWorkingDirPath = Paths.get(mainDirectory);
-        Path absoluteDirectoryPath = currentWorkingDirPath.resolve(subdirectory);
-
         try {
+            Path currentWorkingDirPath = Paths.get(mainDirectory);
+            Path absoluteDirectoryPath = currentWorkingDirPath.resolve(subdirectory);
             return Files.createDirectories(absoluteDirectoryPath);
-        } catch (UnsupportedOperationException | IOException | SecurityException e) {
+
+        } catch (UnsupportedOperationException | IOException | SecurityException  | NullPointerException | InvalidPathException e) {
             log.error("Error creating directory: " + e.getMessage());
             throw new FileCreationException(mainDirectory, subdirectory);
         }
@@ -41,15 +41,11 @@ public class FileUtil {
         try {
             Path sourceFilepath = Paths.get(sourceFile);
             Path sourceFilename = sourceFilepath.getFileName();
-            if (sourceFilename != null && destinationDir != null) {
-                Path destinationDirPath = Paths.get(destinationDir).resolve(sourceFilename.toString());
-                return Files.copy(sourceFilepath, destinationDirPath, StandardCopyOption.REPLACE_EXISTING);
-            } else {
-                throw new FileDownloadException(sourceFile, destinationDir);
-            }
 
+            Path destinationDirPath = Paths.get(destinationDir).resolve(sourceFilename.toString());
+            return Files.copy(sourceFilepath, destinationDirPath, StandardCopyOption.REPLACE_EXISTING);
 
-        } catch (InvalidPathException | IOException ex) {
+        } catch (InvalidPathException | IOException | NullPointerException e) {
             throw new FileDownloadException(sourceFile, destinationDir);
         }
     }
