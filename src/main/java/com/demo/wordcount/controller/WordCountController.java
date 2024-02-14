10d332ff9d6a78318fa3bcac90efb275eaf2cdc9
kate-skip <u5pdAcpLsZF6MfqeYz5T>
2024-02-14 19:16:08 +0800

    added API for GET request

--- src/main/java/com/demo/wordcount/controller/WordCountController.java
+++ src/main/java/com/demo/wordcount/controller/WordCountController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +36,11 @@ public class WordCountController {
             throws IllegalArgumentException, IllegalOperationException {
         List<WordCountDetails> details = wordCountService.count(request);
         return Response.of(new WordCountResponse(details));
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Response<String> welcome() {
+        return Response.of("Hello! Please use a POST request for the word counting. Thanks!");
     }
 }
